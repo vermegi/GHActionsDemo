6c1d2c9bdb5fcd884cf6b9645d90d6abb0c855f1
Gitte Vermeiren <givermei@microsoft.com>
2019-10-25 13:05:58 +0000

    altered welcome msg

--- src/FunctionProject/HttpTrigger/index.js
+++ src/FunctionProject/HttpTrigger/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello super awesome" + (req.query.name || req.body.name)
+            body: "Hello SUPER AWESOME " + (req.query.name || req.body.name)
         };
     }
     else {
